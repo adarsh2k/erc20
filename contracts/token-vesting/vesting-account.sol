03e9d0233dc0f4467080c53241e34120b70b2b47
Adarsh Kumar <adarshkumar2k@gmail.com>
2019-01-23 11:32:44 +0800

    token vesting added

---
+++ contracts/token-vesting/vesting-account.sol
@@ -0,0 +1,32 @@
+pragma solidity ^0.4.24;
+
+import "openzeppelin-solidity/contracts/token/ERC20/TokenVesting.sol";
+import {VestingModel} from "./vesting-model.sol";
+
+contract VestingAccount is TokenVesting
+{
+    VestingModel public vestingModel;
+
+    constructor(address _beneficiary, address _schedule_address, bool _revocable)
+    TokenVesting(_beneficiary, _start, _cliff, _duration, _revocable) public
+    {
+        require(_schedule_address != address(0));
+        vestingModel = VestingModel(_schedule_address);
+    }
+
+
+    function vestedAmount(ERC20Basic token) public view returns (uint256) {
+        uint256 currentBalance = token.balanceOf(this);
+        uint256 totalBalance = currentBalance.add(released[token]);
+
+        if (block.timestamp < (vestingModel.start().add(vestingModel.cliff()))) {
+            return 0;
+        }
+        else if (block.timestamp >= vestingModel.start().add(vestingModel.duration()) || revoked[token]) {
+            return totalBalance;
+        }
+        else {
+            return totalBalance.mul(block.timestamp.sub(vestingModel.start())).div(vestingModel.duration());
+        }
+    }
+}
